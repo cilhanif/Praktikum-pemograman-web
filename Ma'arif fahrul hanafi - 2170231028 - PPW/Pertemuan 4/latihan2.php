dc916f7167803925371fbe11622fcc8d5b8572b7
cilhanif <120603349+cilhanif@users.noreply.github.com>
2022-12-15 04:25:40 +0700

    Add files via upload
    done

---
+++ Ma'arif fahrul hanafi - 2170231028 - PPW/Pertemuan 4/latihan2.php
@@ -0,0 +1,15 @@
+<html>
+    <head>
+        <title>pemrograman PHP dengan array</title>
+    </head>
+    <body>
+        <?php
+        //pertemuan array dapat dibuat seperti berikut
+        $nama[] = "Ma'arif";
+        $nama[] = "Fahrul";
+        $nama[] = "Hanafi";
+        echo $nama[1] . $nama[2] . $nama[0];
+        echo "<br>"
+        ?>
+    </body>
+</html>
